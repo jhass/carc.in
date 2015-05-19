257656560a2cfbc079b23caa45351de9c20ee30a
Jonne Haß <me@jhass.eu>
2015-05-19 20:17:52 +0200

    Hide output section if it has no content and an error is present

---
+++ frontend/app/controllers/run/index.js
@@ -0,0 +1,5 @@
+export default Ember.Controller.extend({
+  displayOutput: function() {
+    return Ember.isPresent(this.get('model.stdout')) || Ember.isBlank(this.get('model.prettyStderr'));
+  }.property('model.stdout', 'model.prettyStderr')
+});
