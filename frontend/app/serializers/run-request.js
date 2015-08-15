3778197a752bb437e2eda44d518e4a34d62b05be
Jonne Haß <me@jhass.eu>
2015-08-15 13:08:36 +0200

    update to ember 1.13.8 and update other frontend dependencies

--- frontend/app/serializers/run-request.js
+++ frontend/app/serializers/run-request.js
@@ -2,6 +2,7 @@ import DS from "ember-data";
 import { ActiveModelSerializer } from 'active-model-adapter';
 
 export default ActiveModelSerializer.extend(DS.EmbeddedRecordsMixin, {
+  isNewSerializerAPI: true,
   attrs: {
     run: {deserialize: 'records', serialize: false}
   }
